d80307a2cc16e50faa1c466822b7c84ddcdd1ba0
Avinash <avinashnm07@gmail.com>
2023-09-18 16:44:51 +0530

    made some styling to new_user.html via new_user.css

--- new_user.php
+++ new_user.php
@@ -3,16 +3,17 @@
 <link rel="stylesheet" type="text/css" href="new_user.css">
 </head>
 <body>
+<h1 class="signup_hdn">SIGN UP</h1>
 <form method="Post">
 <label for="user_name">User Name:</label>
-<input type="text" name="user_name" required title="Please enter your User Name"></br></br>
+<input type="text" class="input" name="user_name" required title="Please enter your User Name"></br></br>
 <label for="email_id">Email ID:</label>
-<input type="email" name="email_id"/></br></br>
+<input type="email" class="input" name="email_id"/></br></br>
 <label for="password">Password:</label>
-<input type="password" name="password"/></br></br>
+<input type="password" class="input" name="password"/></br></br>
 <label for="cnfrm_pw">Confirm Password:</label>
-<input type="password" name="cnfrm_pw"/></br></br>
-<button name="signup_btn">Sign Up</button></br></br>
+<input type="password" class="input" name="cnfrm_pw"/></br></br>
+<button name="signup_btn" class="signup_btn">Sign Up</button></br></br>
 <a href="sign_in.php">Already a user? Sign-in.</a>
 </form>
 </body>
