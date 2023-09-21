b91e0fa8236a155d1ddc23983269446915130e8a
Avinash <avinashnm07@gmail.com>
2023-09-21 10:13:07 +0530

    Changed all the input fields with floating labels

--- new_user.php
+++ new_user.php
@@ -6,7 +6,7 @@
     <script src="Bootstrap/JS/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
-<body>
+<body class="entire-bg">
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
@@ -32,37 +32,56 @@
           <h1 class="signup_hdn">SIGN UP</h1>
         </div>
         <div class="un_section">
-            <label for="user_name">USER NAME:</label>
-            <input type="text" class="input" name="user_name" required title="Please enter your User Name"><br><br>
+          <div class="form-floating mb-3">
+            <input type="text" class="form-control border-0 border-bottom" id="floatingUsername" placeholder="Enter your Username">
+            <label for="floatingUsername">Username</label>
+        </div>
           </div>
             <div class="dob_gender">
                 <div class="form-group dob-group">
-                    <label for="dob">DATE OF BIRTH</label>
-                    <input type="date" name="dob" class="input dob-input" required>
+                  <div class="form-floating mb-3">
+                    <input type="date" class="form-control" id="floatingDOB" placeholder="Enter your DOB">
+                    <label for="floatingDOB">Date Of Birth</label>
+                </div>
                 </div>
                 <div class="form-group gender-group">
-                    <label for="gender">GENDER</label>
-                    <input type="text" name="gender" class="input gender-input" required>
+                  <div class="form-floating">
+                      <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                        <option selected>Select your Gender</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="Other">Other</option>
+                      </select>
+                      <label for="floatingSelect">Gender</label>
+                      </div>
                 </div>
             </div>
             <div class="mail_section">
-            <label for="email_id">Email ID:</label>
-            <input type="email" class="input" name="email_id"><br><br>
+              <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingMail" placeholder="example@gmail.com">
+                <label for="floatingMail">Email address </label>
+            </div>
           </div>
           <div class="pw_section">
-            <label for="password">Password:</label>
-            <input type="password" class="input" name="password"><br><br>
+            <div class="form-floating mb-3">
+              <input type="password" class="form-control" id="floatingPw" placeholder="Enter your Password">
+              <label for="floatingPw">Password</label>
+          </div>
           </div>
           <div class="cpw_section">
-            <label for="cnfrm_pw">Confirm Password:</label>
-            <input type="password" class="input" name="cnfrm_pw"><br><br>
+            <div class="form-floating mb-3">
+              <input type="password" class="form-control" id="floatingCPw" placeholder="Re-enter your Password">
+              <label for="floatingCPw">Confirm Password</label>
           </div>
+          </div>
+          <div class="btn_si">
           <div class="btn_section">
             <button name="signup_btn" class="signup-btn">Sign Up</button><br><br>
           </div>
           <div class="si_section">
             <a href="sign_in.php">Already a user? Sign-in.</a>
           </div>
+        </div>
           </div>
         </form>
     </div>
