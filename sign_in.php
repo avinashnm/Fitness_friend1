a961cc2870b9f48ee5b7f79c9c7e840c35498183
Avinash <avinashnm07@gmail.com>
2023-09-22 23:59:36 +0530

    changes some visual aspects of Sign-in page

--- sign_in.php
+++ sign_in.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="sign_in1.css">
@@ -5,15 +6,56 @@
     <script src="Bootstrap/JS/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
-<body class="body">
-<form method="Post">
-<label for="user_name">User Name:</label>
-<input type="text" name="user_name"/></br></br>
-<label for="password">Password:</label>
-<input type="password" name="password"/></br></br>
-<button name="signup_btn">Sign Up</button></br></br>
-<a href="/php_project/new_user.php">New User? Sign-up.</a>
-</form>
+<body class="entire-bg">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="logo1.png" alt="FitGen Logo" width="50" height="44">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link active" href="new_user.php">SIGN UP</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+    <div class="signin_fullsection">
+    <div class="signin_section">
+        <form method="Post" class="signin_form">
+          <div class="hdn_section">
+          <h1 class="signin_hdn">SIGN IN</h1>
+        </div>
+        <div class="un_section">
+          <div class="form-floating mb-3">
+            <input type="text" class="form-control border-0 border-bottom" id="floatingUsername" placeholder="Enter your Username">
+            <label for="floatingUsername">Username</label>
+        </div>
+          </div>
+          <div class="pw_section">
+            <div class="form-floating mb-3">
+              <input type="password" class="form-control" id="floatingPw" placeholder="Enter your Password">
+              <label for="floatingPw">Password</label>
+          </div>
+          </div>
+          <div class="btn_si">
+          <div class="btn_section">
+            <button name="signin_btn" class="signin_btn">Sign In</button><br><br>
+            <div class="btn_section">
+              <button name="forgotpw_btn" class="forgotpw_btn">Forgot Password</button><br><br>
+          </div>
+          <div class="su_section">
+            <a href="new_user.php">New User? Sign-up.</a>
+          </div>
+        </div>
+          </div>
+        </form>
+    </div>
 </body>
 </html>
 <?php
@@ -23,8 +65,6 @@ $Password="";
 $Dbname = "Fitness_Friend";
 
 $conn = mysqli_connect($ServerName, $Username, $Password, $Dbname);
-
-
 
 
 ?>
