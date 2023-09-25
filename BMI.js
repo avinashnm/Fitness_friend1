5e0ab0c9468854f95139830ce280558346e5c7d8
Avinash <avinashnm07@gmail.com>
2023-09-26 04:07:45 +0530

    BMI is displaying as expected

--- BMI.js
+++ BMI.js
@@ -1,9 +1,20 @@
 function validateWeightHeightForm() {
-    var weight = document.getElementById("weight").value;
-    var height = document.getElementById("height").value;
-    if (weight === "" || height === "") {
-        alert("Enter all the required fields *");
+    var weight = parseFloat(document.getElementById("weight").value);
+    var heightInCm = parseFloat(document.getElementById("height").value);
+
+    if (isNaN(weight) || isNaN(heightInCm) || weight <= 0 || heightInCm <= 0) {
+        alert("Please enter valid weight and height values.");
         return false;
     }
+
+    // Convert height from cm to meters
+    var heightInM = heightInCm / 100;
+
+    // Calculate BMI
+    var bmi = weight / (heightInM * heightInM);
+
+    // Display the BMI
+    document.getElementById("bmiResult").textContent = "Your BMI is: " + bmi.toFixed(2);
+
     return true;
 }
