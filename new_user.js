83d6f04eeaf584528be79e0fedf520c3f423224e
Avinash <avinashnm07@gmail.com>
2023-09-25 11:45:04 +0530

    Enhanced the validation in new_user.html

--- new_user.js
+++ new_user.js
@@ -11,29 +11,37 @@ function validateForm() {
     var password = document.getElementById("floatingPw").value;
     var confirm_password = document.getElementById("floatingCPw").value;
 
-    // Check if any of the fields are empty
-    if (
-        full_name === "" ||
-        dob === "" ||
-        gender === "Select your Gender" ||
-        weight === "" ||
-        height === "" ||
-        activity_level === "Select your Activity Level" ||
-        fitness_goal === "Choose your Fitness Goal" ||
-        username === "" ||
-        email === "" ||
-        password === "" ||
-        confirm_password === ""
-    ) {
-        alert("All fields are required.");
-        return false;
-    }
+    // Perform AJAX request to submit the form data to PHP for validation
+    $.ajax({
+        type: "POST",
+        url: "new_user.php", // Replace with the actual URL of your PHP script
+        data: {
+            full_name: full_name,
+            dob: dob,
+            gender: gender,
+            weight: weight,
+            height: height,
+            activity_level: activity_level,
+            fitness_goal: fitness_goal,
+            username: username,
+            email: email,
+            password: password,
+            confirm_password: confirm_password
+        },
+        success: function (response) {
+            // Parse the JSON response
+            var responseData = JSON.parse(response);
 
-    // Check if passwords match
-    if (password !== confirm_password) {
-        alert("Password and Confirm Password do not match.");
-        return false;
-    }
+            if (responseData.status === "success") {
+                // Registration was successful, you can redirect or display a success message
+                alert(responseData.message);
+                // Redirect to a success page or perform other actions
+                window.location.href = "success_page.php";
+            } else {
+                // Display the error message
+                alert(responseData.message);
+            }
+        }
+    });
 
-    return true;
-}
+    return false; //
