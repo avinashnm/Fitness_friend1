cad1ef45b7dfb08327fdad93c24ad926a1dfe421
Avinash <avinashnm07@gmail.com>
2023-09-25 11:15:59 +0530

    enhanced the form validation using new_user.js

--- new_user.js
+++ new_user.js
@@ -1,13 +1,39 @@
-// Get the two steps of the sign up form
-const step1 = document.getElementById('step-1');
-const step2 = document.getElementById('step-2');
+function validateForm() {
+    var full_name = document.getElementById("floatingUsername").value;
+    var dob = document.getElementById("floatingDOB").value;
+    var gender = document.getElementById("floatingSelectGender").value;
+    var weight = document.getElementById("floatingWeight").value;
+    var height = document.getElementById("floatingHeight").value;
+    var activity_level = document.getElementById("floatingSelectActivity").value;
+    var fitness_goal = document.getElementById("floatingSelectGoal").value;
+    var username = document.getElementById("floatingUsername2").value;
+    var email = document.getElementById("floatingMail").value;
+    var password = document.getElementById("floatingPw").value;
+    var confirm_password = document.getElementById("floatingCPw").value;
 
-// Toggle the visibility of the two steps
-function toggleSteps() {
-  step1.classList.toggle('hidden');
-  step2.classList.toggle('hidden');
+    // Check if any of the fields are empty
+    if (
+        full_name === "" ||
+        dob === "" ||
+        gender === "Select your Gender" ||
+        weight === "" ||
+        height === "" ||
+        activity_level === "Select your Activity Level" ||
+        fitness_goal === "Choose your Fitness Goal" ||
+        username === "" ||
+        email === "" ||
+        password === "" ||
+        confirm_password === ""
+    ) {
+        alert("All fields are required.");
+        return false;
+    }
+
+    // Check if passwords match
+    if (password !== confirm_password) {
+        alert("Password and Confirm Password do not match.");
+        return false;
+    }
+
+    return true;
 }
-
-// Add event listeners to the next and previous buttons
-document.querySelector('.nextStep').addEventListener('click', toggleSteps);
-document.querySelector('.prevStep').addEventListener('click', toggleSteps);
